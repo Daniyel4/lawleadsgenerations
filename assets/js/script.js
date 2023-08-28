4df9ea1092a520be8d31c45bf358e7d7dc244d3c
Daniyel4 <88538591+Daniyel4@users.noreply.github.com>
2023-08-28 16:20:52 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(".mobile-menu").hide()
+
+$(".navbar-open").on("click", function(){
+    
+    $(".mobile-menu").show()
+});
+
+
+
+$(".close-btn").on("click", function(){
+    
+    $(".mobile-menu").hide()
+});
